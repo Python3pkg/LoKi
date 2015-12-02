3fd20a5ad10103e6fe39b002c4705647f742da13
Chris Theissen <ctheissen@gmail.com>
2015-12-02 12:56:51 -0800

    added constants

---
+++ slw_constants.py
@@ -0,0 +1,7 @@
+Rsun      = 8500.            # R coord for the Sun in parsecs
+Tsun      = 0.               # theta coord for the Sun in parsecs
+Zsun      = 15.              # Z coord for the Sun in parsecs
+rho0      = 0.0064           # rho0 from Juric et al. (2008)
+au2pc     = 1 / 206264.806   # conversion from AU to parsecs
+cell_size = 0.5              # size of one cell in degrees
+max_dist  = 2500.            # maximum allowed distance for simulated star
